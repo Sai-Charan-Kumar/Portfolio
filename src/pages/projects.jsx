870151e6b4ba2aaf206c5186cfe08a95ddfc2946
Sai-Charan-Kumar <msaicharankumar@gmail.com>
2026-08-14 15:13:13 +0530

    Refactor Projects component to remove theme prop
    Removed theme prop from ProjectList component.

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ProjectList from '../components/ProjectList';
 import { projectsData } from '../data/projects';
 
-function Projects({ theme }) {
+function Projects() {
   return (
     <div className="projects-page">
       <h2 className="page-title">Featured Projects</h2>
-      <ProjectList projects={projectsData} theme={theme} />
+      <ProjectList projects={projectsData}/>
     </div>
   );
 }
